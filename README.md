dcc29cd6fae8517b72c29c00bf0d1c469d01be62
Shiv19 <mailshiva19@gmail.com>
2017-10-22 18:45:25 +0530

    added documentation

--- README.md
+++ README.md
@@ -1,5 +1,28 @@
+# nativescript-rivescript-demo
+[![Twitter URL](https://img.shields.io/badge/twitter-%40MultiShiv19-blue.svg)](https://twitter.com/MultiShiv19)
 
-### What is this repository for? ###
 
-* This repo contains the NativeScript App for AnyGoUser
-* Version - 0.1.0
+This repo is a demo to show the power of NativeScript + RiveScript
+
+## Screenshots
+
+<img src="assets/demo.jpg?raw=true" height="320" > 
+
+## Installation
+
+```javascript
+npm i
+
+tns run android (or ios)
+```
+
+## Usage 
+
+Just chat with that bot, using the chat box.
+
+## Server Code
+The server can be found at <a href="https://github.com/shiv19/node-rivebot" target="_blank">this repository</a>
+    
+## License
+
+MIT
--- app/main-view-model.js
+++ app/main-view-model.js
@@ -1,22 +1,59 @@
-var Observable = require("data/observable").Observable;
-
-function getMessage(counter) {
-    if (counter <= 0) {
-        return "Hoorraaay! You unlocked the NativeScript clicker achievement!";
-    } else {
-        return counter + " taps left";
-    }
-}
+const Observable = require("data/observable").Observable;
+const ObservableArray = require("data/observable-array").ObservableArray;
+const fetchModule = require("fetch");
+const host = "https://rivebot.shiv19.com";
 
 function createViewModel() {
-    var viewModel = new Observable();
-    viewModel.counter = 42;
-    viewModel.message = getMessage(viewModel.counter);
+    const viewModel = new Observable();
+    viewModel.userMsg = "";
+    viewModel.chats = new ObservableArray([]);
 
-    viewModel.onTap = function() {
-        this.counter--;
-        this.set("message", getMessage(this.counter));
-    }
+    viewModel.onChat = function() {
+        viewModel.chats.push({
+            "who": "user",
+            "image": "~/assets/user.png",
+            "message": viewModel.userMsg,
+            "timestamp": "12:03 PM"
+        });
+        fetchModule.fetch(host + "/reply", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                "username":"user",
+                "message":viewModel.userMsg
+            })
+        })
+        .then((response) => {
+            if (!response.ok) {
+                const responseObj = response.json();
+                if (!responseObj.error) {
+                    responseObj.error = "Something went wrong!";
+                }
+
+                return responseObj;
+            }
+
+            return response.json();
+        })
+        .then((response) => {
+            const botReply = {
+                "who": "bot",
+                "image": "~/assets/bot.png",
+                "message": "",
+                "timestamp": "12:03 PM"
+            };
+            if (response.error) {
+                console.log("couldn't talk to bot");
+                botReply.message = "Something went wrong";
+            }
+            else {
+                botReply.message = response.reply;
+            }
+            viewModel.chats.push(botReply);
+        });
+    };
 
     return viewModel;
 }
